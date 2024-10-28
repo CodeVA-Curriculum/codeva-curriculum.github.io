b6787e7155d0be762879d42bf7bd7c936f5af24b
Jon Stapleton <stapleton.jon.michael@gmail.com>
2024-10-28 09:21:08 -0400

    added vocab cards

---
+++ _app/immutable/chunks/stores.95e9ab36.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.8a0729ac.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
